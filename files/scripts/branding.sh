e1cff5b8a98d77477fef0ef38cacc5276f2d16d6
julia <sea.leaf@outlook.com>
2024-10-26 19:39:08 +0800

    updated branding.sh

--- files/scripts/branding.sh
+++ files/scripts/branding.sh
@@ -2,6 +2,6 @@
 set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
-sed -i 's/bluefin-dx/sharkfin/g' /usr/share/ublue-os/image-info.json
-sed -i 's/ublue-os/vibrantleaf/g' /usr/share/ublue-os/image-info.json
-sed -i 's/ghcr.io/ublue-os/bluefin-dx/ghcr.io/vibrantlef/sharkfin/g' /usr/share/ublue-os/image-info.json
+sed -i 's@bluefin-dx@sharkfin@g' /usr/share/ublue-os/image-info.json
+sed -i 's@ublue-os@vibrantleaf@g' /usr/share/ublue-os/image-info.json
+sed -i 's@ghcr.io/ublue-os/bluefin-dx@ghcr.io/vibrantlef/sharkfin@g' /usr/share/ublue-os/image-info.json
